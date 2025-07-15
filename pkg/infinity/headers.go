f50a1c178bb2c7241a59a32941e6b6aea0c81211
tienthai-ammp <tien.thai@ammp.io>
2025-07-15 20:47:07 +0200

    fix: add nil check and empty check when setting grafana user header

--- pkg/infinity/headers.go
+++ pkg/infinity/headers.go
@@ -151,6 +151,8 @@ func ApplyForwardedOAuthIdentity(requestHeaders map[string]string, settings mode
 }
 
 func SetGrafanaUserHeader(query querySrv.Query, req *http.Request) *http.Request {
-	req.Header.Set(headerKeyGrafanaUser, query.GrafanaUser)
+	if query.GrafanaUser != "" {
+		req.Header.Set(headerKeyGrafanaUser, query.GrafanaUser)
+	}
 	return req
 }
